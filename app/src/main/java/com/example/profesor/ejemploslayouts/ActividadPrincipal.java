aec9fdd776a3dd2f64792931945a934c0b075e62
Jose M <jmdh@aytoalcobendas.org>
2018-09-07 13:25:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/profesor/ejemploslayouts/ActividadPrincipal.java
@@ -0,0 +1,15 @@
+package com.example.profesor.ejemploslayouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ActividadPrincipal extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejemplo_relative_layout);
+    }
+}
