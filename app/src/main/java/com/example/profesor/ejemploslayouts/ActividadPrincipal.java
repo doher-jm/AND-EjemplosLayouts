1f13fb6923140beea529f428eff16d3bc79e9df4
jmdomher <jmdominguez@aytoalcobendas.org>
2018-09-09 19:50:12 +0200

    Rev. 1

--- app/src/main/java/com/example/profesor/ejemploslayouts/ActividadPrincipal.java
+++ app/src/main/java/com/example/profesor/ejemploslayouts/ActividadPrincipal.java
@@ -1,5 +1,6 @@
 package com.example.profesor.ejemploslayouts;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +11,7 @@ public class ActividadPrincipal extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Ir sustituyendo aquí cada id de fichero de recurso layout a mostar
         setContentView(R.layout.ejemplo_relative_layout);
     }
 }
